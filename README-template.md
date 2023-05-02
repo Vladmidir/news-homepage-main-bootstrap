61c1521dcb06a5147e63719dff54300433b86506
Vladmidir <vladgoran2004@gmail.com>
2023-05-02 03:30:07 -0400

    finished mobile. Just gotta fix some bugs now

--- README-template.md
+++ README-template.md
@@ -84,6 +84,8 @@ If you want more help with writing markdown, we'd recommend checking out [The Ma
 **Note: Delete this note and the content within this section and replace with your own learnings.**
 
 ### Continued development
+Could not figure out how to use pseudoclass for overlay 
+
 
 Use this section to outline areas that you want to continue focusing on in future projects. These could be concepts you're still not completely comfortable with or techniques you found useful that you want to refine and perfect.
 
--- scripts.js
+++ scripts.js
@@ -1,7 +1,20 @@
 function openMenu() {
-    document.getElementById("open-button").classList.add("d-none")
-}
+    document.getElementById("open-button").style.display = "none";
 
+    document.getElementById("nav-links").style.width = "200px";
+    document.getElementById("close-button").style.display = "inline";
+
+    document.getElementById("overlay-div").style.width = "100%";
+    //This breaks the nav. When we go back to large screen, the styles are all messed up.
+
+}
+//Add aria-expended?
 function closeMenu() {
+    document.getElementById("open-button").style.display = "inline";
+    document.getElementById("close-button").style.display = "none";
+
+    document.getElementById("overlay-div").style.width = "0";
+
+    document.getElementById("nav-links").style.width = "0";
 
 }
